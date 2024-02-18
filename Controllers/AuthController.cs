e596329e8f712540e17d9a9143cf906a51290a3d
Cristian Henao <cristian.henao@siesa.com>
2024-02-18 15:12:01 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using HotelAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost(Name = "LoginHotelApp")]
+        public SignInModel LoginHotelApp(SignInModel model)
+        {
+            SignInModel signInModel = new SignInModel();
+            if (!string.IsNullOrEmpty(model.UserName) || !string.IsNullOrEmpty(model.Password))
+            {
+                //Valores no son nulos o no estan vacios
+
+                if (model.UserName == "Administrador" && model.Password == "Admin2024.")
+                {
+                    //Paso la prueba, el login es valido
+                    signInModel.UserName = model.UserName;
+                }
+                else
+                {
+                    signInModel.Errors = new List<string> { "El usuario o la contraseña son incorrectos, revise los datos o comuniquese con el administrador." };
+                }
+            }
+            else
+            {
+                signInModel.Errors = new List<string> { "El usuario y la contraseña son obligatorios." };
+            }
+
+            return signInModel;
+        }
+    }
+}
---
+++ Models/SignInModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelAPI.Models
+{
+    public class SignInModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
